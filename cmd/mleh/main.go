bc904d73b191415f16b1778d25f753645abdda36
Ivan Valdes (@ivanvc) <iv@a.ki>
2020-11-19 13:30:22 -0800

    Support values through arguments and dry mode

--- cmd/mleh/main.go
+++ cmd/mleh/main.go
@@ -17,12 +17,26 @@ type TemplateInput struct {
 	Values map[interface{}]interface{}
 }
 
+type valuesFlag []string
+
+func (v valuesFlag) String() string {
+	return strings.Join(v, ", ")
+}
+
+func (v *valuesFlag) Set(value string) error {
+	*v = append(*v, value)
+	return nil
+}
+
 var (
-	values    = flag.String("values", "", "The values.yaml file")
-	outputDir = flag.String("output-dir", "", "The output directory")
+	valuesFile = flag.String("values", "", "The values.yaml file")
+	outputDir  = flag.String("output-dir", "", "The output directory")
+	dryMode    = flag.Bool("dry", false, "Run in dry mode")
+	values     valuesFlag
 )
 
 func init() {
+	flag.Var(&values, "value", "Options to be used as values, as key=value where value is in YAML format")
 	flag.Parse()
 }
 
@@ -36,18 +50,38 @@ func main() {
 	if err != nil {
 		log.Fatal("Error reading chart values.yaml: ", err.Error())
 	}
-	yaml.Unmarshal(dat, &ti.Values)
-
-	dat, err = ioutil.ReadFile(*values)
-	if err != nil {
-		log.Fatal("Error reading chart values.yaml: ", err.Error())
+	if err = yaml.Unmarshal(dat, &ti.Values); err != nil {
+		log.Fatal("Error parsing chart values.yaml: ", err.Error())
 	}
-	yaml.Unmarshal(dat, &ti.Values)
 
-	tpl := template.Must(template.New("base").Funcs(sprig.TxtFuncMap()).ParseGlob(filepath.Join(inputDir, "templates/[^.]*")))
+	if len(*valuesFile) > 0 {
+		dat, err = ioutil.ReadFile(*valuesFile)
+		if err != nil {
+			log.Fatal("Error reading input values yaml file: ", err.Error())
+		}
+		if err = yaml.Unmarshal(dat, &ti.Values); err != nil {
+			log.Fatal("Error parsing input values yaml file: ", err.Error())
+		}
+	}
 
-	if err := os.MkdirAll(*outputDir, 0755); err != nil {
-		log.Fatal("Error creating output diirectory: ", err.Error())
+	for _, v := range values {
+		var value interface{}
+		key := strings.Split(v, "=")[0]
+		v = strings.TrimPrefix(v, key+"=")
+		if err = yaml.Unmarshal([]byte(v), &value); err != nil {
+			log.Fatal("Error parsing ", key, " with value ", v, ": ", err.Error())
+		}
+		ti.Values[key] = value
+	}
+
+	tpl := template.Must(template.New("base").Funcs(sprig.TxtFuncMap()).ParseGlob(filepath.Join(inputDir, "templates/*")))
+
+	if *dryMode {
+		log.Println("Dry mode, skipping creating directory ", *outputDir)
+	} else {
+		if err := os.MkdirAll(*outputDir, 0755); err != nil {
+			log.Fatal("Error creating output diirectory: ", err.Error())
+		}
 	}
 
 	for _, t := range tpl.Templates() {
@@ -56,6 +90,9 @@ func main() {
 		}
 
 		fn := filepath.Join(*outputDir+"/", t.Name())
+		if *dryMode {
+			fn = os.DevNull
+		}
 		f, err := os.Create(fn)
 		if err != nil {
 			log.Fatal("Error creating file ", fn, ": ", err.Error())
